f8589cd163fb5d8ab5a066737529eaa0cc941f6d
ajmeriunnisa <ajmeriunnisa877@gmail.com>
2025-10-22 17:30:20 +0530

    added styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+function Header() {
+  return (
+    <div>
+      <header className='header'>To-Do List</header>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import "./style.css"
+
+function ToDoItem({ task, onDelete, onToggle, onEdit }) {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editText, setEditText] = useState(task.text);
+
+
+    const handleEdit = () => {
+        if (isEditing) {
+            onEdit(task.id, editText);
+        }
+        setIsEditing(!isEditing);
+    };
+
+    return (
+        <li className='todo-item'>
+            {isEditing ? (
+                <input
+                    type="text"
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                />
+            ) : (
+                <div className="task-text">
+                    <input
+                        type="checkbox"
+                        checked={task.completed}
+                        onChange={() => onToggle(task.id)}
+                    />
+                    {isEditing ? (
+                        <input
+                            type="text"
+                            value={editText}
+                            onChange={(e) => setEditText(e.target.value)}
+                        />
+                    ) : (
+                        <span className={task.completed ? "completed" : ""}>
+                            {task.text}
+                        </span>
+                    )}
+                </div>
+            )
+            }
+
+            <div>
+                <button className='edit-btn' onClick={handleEdit}>
+                    {isEditing ? "Save" : "Edit"}
+                </button>
+
+                <button className='delete-btn' onClick={() => onDelete(task.id)}>
+                    Delete
+                </button>
+            </div>
+        </li >
+    );
+}
+
+export default ToDoItem
