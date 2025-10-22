362abe9ec8ea17172d234f0216ffb392d5201ec0
ajmeriunnisa <ajmeriunnisa877@gmail.com>
2025-10-22 16:04:46 +0530

    Implemented ToDOList and TODOItem

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
+import ToDoList from './components/ToDoList'
+import "./components/style.css";
 
 function App() {
+
+  const [tasks,setTasks]=useState([]);
+  const [newTask,setNewTask]=useState("");
+
+  const handleAddTask=()=>{
+    if(newTask.trim()==="")return;
+    setTasks([
+      ...tasks,
+      {id:Date.now(),text:newTask,completed:false}
+    ]);
+    setNewTask("");
+  };
+
+  const handleDeleteTask=(id)=>{
+    setTasks(tasks.filter((task)=>task.id!==id));
+  };
+
+  const handleToggleComplete=(id)=>{
+    setTasks(
+      tasks.map((task)=>
+      task.id===id ? {...task,completed : !task.completed} : task
+      )
+    );
+  };
+
+  const handleEditTask=(id,newText)=>{
+    setTasks(
+      tasks.map((task)=>task.id===id ? {...task,text:newText} : task
+    )
+    );
+  };
+
   return (
-    <div>
+    <div className='app-container'>
       <Header/>
+      <div className='todo-box'>
+        <div className='input-container'>
+          <input 
+            type='text'
+            placeholder='Enter new Task...'
+            value={newTask}
+            onChange={(e)=>setNewTask(e.target.value)}
+            />
+            <button onClick={handleAddTask}>
+              Add
+            </button>
+        </div>
+
+        <ToDoList 
+          tasks={tasks}
+          onDelete={handleDeleteTask}
+          onToggle={handleToggleComplete}
+          onEdit={handleEditTask}
+        />
+      </div> 
     </div>
   )
 }
---
+++ todo-app/src/components/ToDoItem.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import "./style.css"
+
+function ToDoItem({task,onDelete,onToggle,onEdit}) {
+
+    const [isEditing,setIsEditing]=useState(false);
+    const [editText,setEditText]=useState(task.text);
+
+
+    const handleEdit=()=>{
+        if(isEditing){
+            onEdit(task.id,editText);
+        }
+        setIsEditing(!isEditing);
+    };
+
+  return (
+    <li className='todo-item'>
+        {isEditing ?(
+        <input 
+        type="text" 
+        value={editText}
+        onChange={(e)=>setEditText(e.target.value)}
+        />
+        ) : (
+            <span 
+                className={task.completed ? "completed":""}
+                onClick={()=>onToggle(task.id)}
+            >
+                {task.text}
+            </span>
+        )}
+
+        <div>
+            <button className='edit-btn' onClick={handleEdit}>
+                {isEditing ? "Save" : "Edit"}
+            </button>
+
+            <button className='delete-btn' onClick={()=>onDelete(task.id)}>
+                Delete
+            </button>
+        </div>
+    </li>
+  );
+}
+
+export default ToDoItem
